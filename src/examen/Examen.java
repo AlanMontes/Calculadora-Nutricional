ab9a027a9fe4170aa7a95203063f5a99e0c3a6e0
52614 <52614@LAPTOP-ACCAD5FF>
2021-04-21 20:32:19 -0600

    Prueba 2

--- src/examen/Examen.java
+++ src/examen/Examen.java
@@ -1,20 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- Hola soy el Alan
- */
+
+
 package examen;
 
-/**
- *
- * @author 52614
- */
+
 public class Examen {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
         // TODO code application logic here
      //Hola soy german y te apuesto una naranja a que lo primero que haces al despertar es abrir los ojos
