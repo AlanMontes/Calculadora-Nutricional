1433e60f0ab03da3752f190d1cd11f0f114d082c
oqekr <oqekr@CerealKiller22>
2021-04-22 13:59:06 -0600

    el pepe

--- src/examen/Examen.java
+++ src/examen/Examen.java
@@ -7,6 +7,8 @@ public class Examen {
     public static void main(String[] args) {
         Persona p1 = new Persona();
         p1.getPersona();
+        Ventana V = new Ventana();      // creamos una ventana
+        V.setVisible(true);             // hacemos visible la ventana creada
 
     }
     
